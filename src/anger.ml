96fccb466b75308725e2d79604e2f51e51fd9ba5
Daniel Hines <d4hines@gmail.com>
2022-05-23 15:10:58 -0400

    wip

--- src/anger.ml
+++ src/anger.ml
@@ -3,21 +3,39 @@ open Feather.Infix
 
 let default_remote = "origin"
 let default_branch = "master"
+let branch_prefix = "d4hines"
+let git = process "git"
 
+let name = Stdlib.read_line ()
+
+let () = print_endline @@ "Hello, " ^ name
+
+(* Find all commits between HEAD and origin/main *)
 let all_commits =
-  process "git"
-    [ "rev-list"; Format.sprintf "%s/%s..HEAD" default_remote default_branch ]
+  git [ "rev-list"; Format.sprintf "%s/%s..HEAD" default_remote default_branch ]
   |> collect stdout |> lines
 
-let all_notes =
+  
+(* Gather up the notes for each commit *)
+let revs =
   all_commits
   |> List.map (fun rev ->
-         process "git" [ "notes"; "show"; rev ] |> collect stdout)
+         git [ "notes"; "show"; rev ]
+         (* I think stderr is safe here to drop in /dev/null*)
+         >! "/dev/null"
+         |> collect stdout_and_status)
+  |> List.map (fun (notes, status) ->
+    if status = 0 then notes else
+      print_endline "No "
+      let branch_name = 
+    if status = 0 then Some notes else None)
 
-let () = List.iter (fun notes -> Format.printf "note: %s\n" notes) all_notes
+(* let () =
+  List.iter (function None -> git [ "branch" ] | Some notes -> assert false) *)
+(*  *)
 
 (*
-   find all commits between HEAD and origin/main
+   
    > git rev-list origin/main..HEAD
 
    check their annotations
@@ -36,4 +54,6 @@ let () = List.iter (fun notes -> Format.printf "note: %s\n" notes) all_notes
      > git branch -f <generated name>
      push the changes
      > git push --force-with-lease --set-upstream origin <generated name>
-*)
+ *)
+
+(* test edit *)
